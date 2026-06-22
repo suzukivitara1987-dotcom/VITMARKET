3bffca867169825ce6b84b271dfa2917ad479124
suzukivitara1987-dotcom <suzukivitara1987@gmail.com>
2026-06-22 08:28:17 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "vitmarket-kz.firebaseapp.com",
+  projectId: "vitmarket-kz",
+  storageBucket: "vitmarket-kz.firebasestorage.app",
+  messagingSenderId: "690359698157",
+  appId: "1:690359698157:web:3c8c89253faaefed9f42e4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export const CLOUD_NAME = "dijdrdudb";
+export const UPLOAD_PRESET = "VITMARKET";

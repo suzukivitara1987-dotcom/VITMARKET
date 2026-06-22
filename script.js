487ede672d31ef4977cc5ed57d257282244b448e
suzukivitara1987-dotcom <suzukivitara1987@gmail.com>
2026-06-22 19:16:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function playClick() {
     clickSound.currentTime = 0;
     clickSound.play();
 }
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelectorAll("button, a").forEach(element => {
+
+    element.addEventListener("click", () => {
+      playClick();
+    });
+
+  });
+
+});
